608c7d3e626a9a4e554e13dfc40ca11ae936a7c6
lazuskica <lazuskica@gmail.com>
2024-08-13 18:26:57 +0200

    zapravo dodato sad

---
+++ Core/Inc/lib.h
@@ -0,0 +1,33 @@
+/*
+ * lib.h
+ *
+ *  Created on: Aug 13, 2024
+ *      Author: lazar
+ */
+
+#ifndef LIB_LIB_H_
+#define LIB_LIB_H_
+
+void
+pwm_right_dc (int16_t duty_cycle);
+void
+pwm_left_dc (int16_t duty_cycle);
+void
+pwm_start ();
+void
+pwm_stop ();
+
+void
+time_ISR ();
+uint8_t
+delay_nonblocking (uint32_t delay_ms);
+void
+time_start ();
+void
+time_stop ();
+void
+start_match();
+void
+stop_match();
+
+#endif /* LIB_LIB_H_ */
