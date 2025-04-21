147bfc477e604b0d0e60fb2c439f562f899957ba
LeaHercek <herceklea@gmail.com>
2025-04-21 20:13:56 +0200

    dodatak

--- Core/Lib/tasks/task_sprat_3.c
+++ Core/Lib/tasks/task_sprat_3.c
@@ -49,7 +49,7 @@ task_sprat_3 (int8_t side)
 			break;
 
 		case 50:
-			cur_task = move_on_dir (195, side, 0.2, NO_SENS);
+			cur_task = move_on_dir (205, side, 0.2, NO_SENS);
 			if (cur_task == TASK_SUCCESS)
 				task_fsm_case = 60;
 			break;
