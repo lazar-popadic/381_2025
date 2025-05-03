c3441b2f7c6ef943776545c33f4ddbf8246f1e84
antonicm <mantonic040@gmail.com>
2025-05-03 21:39:19 +0200

    plava taktika 1 na boljem stolu prva alternativna, 95s

--- Core/Lib/tactics/tact_1.c
+++ Core/Lib/tactics/tact_1.c
@@ -90,9 +90,9 @@ int8_t tact_1() {
 		break;
 	case 70:
 		if (get_tact_num_ptr()->side)	// plava
-			cur_task =  move_on_dir(450, FORWARD, 0.2, NO_SENS); //280
+			cur_task =  move_on_dir(450, FORWARD, 0.5, NO_SENS); //280
 		else
-			cur_task = move_on_dir(450, BACKWARD, 0.2, NO_SENS);
+			cur_task = move_on_dir(450, BACKWARD, 0.5, NO_SENS);
 		if (cur_task == TASK_SUCCESS)
 			tact_fsm_case = 80;
 		break;
@@ -128,7 +128,7 @@ int8_t tact_1() {
 		if (cur_task == TASK_SUCCESS)
 			tact_fsm_case = 95;
 		if (timeout (2000))
-						tact_fsm_case = 500;
+			tact_fsm_case = 500;
 		break;
 	case 95:
 		cur_task = rot_to_phi(180, W_MAX_DEF, NO_SENS); //180
@@ -298,6 +298,131 @@ int8_t tact_1() {
 		//		if (cur_task == TASK_SUCCESS)
 		//			tact_fsm_case = 9999;
 		//		break;
+	case 500:
+		grtl_back_close ();
+		HAL_Delay (10);
+		grtl_front_close ();
+		tact_fsm_case = 510;
+		break;
+	case 510: //rezervisan bunt
+		if (get_tact_num_ptr()->side)	// plava
+			cur_task = move_to_xy(x_side(-705), 400, FORWARD, V_MAX_DEF*0.5, W_MAX_DEF*0.5, FORWARD);
+		else
+			cur_task = move_to_xy(x_side(-665), 400, BACKWARD, V_MAX_DEF*0.5, W_MAX_DEF*0.5, BACKWARD);
+
+		if (cur_task == TASK_SUCCESS)
+			tact_fsm_case = 520;
+		break;
+	case 520:
+		if (get_tact_num_ptr()->side)	// plava
+		{
+			cur_task = rot_to_phi(phi_side(90), W_MAX_DEF, NO_SENS);
+			prepare_front();
+		} else {
+			cur_task = rot_to_phi(phi_side(-90), W_MAX_DEF, NO_SENS);
+			prepare_back();
+		}
+		if (cur_task == TASK_SUCCESS)
+			tact_fsm_case = 530;
+		break;
+
+	case 530:
+		if (get_tact_num_ptr()->side)	// plava
+			cur_task = move_on_dir(185, FORWARD, 0.2, NO_SENS);//140
+		else
+			cur_task = move_on_dir(140, BACKWARD, 0.2, NO_SENS);
+
+		if (cur_task == TASK_SUCCESS)
+			tact_fsm_case = 540;
+		break;
+	case 540:
+		if (get_tact_num_ptr()->side) {
+			grtl_front_grip_all();
+			ruc_front_carry();
+			lift_front_carry();
+			vacuum_front(1);
+		} else {
+			grtl_back_grip_all();
+			ruc_back_carry();
+			lift_back_carry();
+			vacuum_back(1);
+		}
+		tact_fsm_case = 550;
+	case 550:
+		if (get_tact_num_ptr()->side)	// plava
+			cur_task = move_on_dir(420, BACKWARD, 0.5, BACKWARD);//140
+		else
+			cur_task = move_on_dir(140, FORWARD, 0.5, FORWARD);
+
+		if (cur_task == TASK_SUCCESS)
+			tact_fsm_case = 560;
+		break;
+	case 560: //bunt kod protivnickog sa druge strane  gore
+		if (get_tact_num_ptr()->side)	// plava
+			cur_task = move_to_xy(x_side(1100), 350, BACKWARD, 0.75, //-1182
+					W_MAX_DEF,
+					BACKWARD);
+		else
+			cur_task = move_to_xy(x_side(1182), 582, FORWARD, V_MAX_DEF,
+					W_MAX_DEF,
+					FORWARD);
+
+		if (cur_task == TASK_SUCCESS)
+			tact_fsm_case = 570;
+		break;
+	case 570:
+		prepare_back();
+		tact_fsm_case = 575;
+		break;
+	case 575:
+		cur_task = rot_to_phi(phi_side(180), W_MAX_DEF, NO_SENS);
+		if (cur_task == TASK_SUCCESS)
+			tact_fsm_case = 580;
+		break;
+	case 580: //pred uzimanje bunta
+		cur_task = move_on_dir(150, BACKWARD, 0.2, NO_SENS); //280
+		if (cur_task == TASK_SUCCESS)
+			tact_fsm_case = 590;
+		break;
+	case 590:
+		grtl_back_grip_all();
+		ruc_back_carry();
+		lift_back_carry();
+		vacuum_back(1);
+		tact_fsm_case = 595;
+		break;
+	case 595:
+		cur_task = move_on_dir(50, FORWARD, 0.2, NO_SENS); //280
+		if (cur_task == TASK_SUCCESS)
+			tact_fsm_case = 600;
+		break;
+	case 600:
+			cur_task = move_to_xy(x_side(950), -100, FORWARD, V_MAX_DEF*0.75, W_MAX_DEF*0.5, NO_SENS);
+			if (cur_task == TASK_SUCCESS)
+				tact_fsm_case = 610;
+			break;
+	case 610:
+		cur_task = rot_to_phi(phi_side(180), W_MAX_DEF, NO_SENS);
+		if (cur_task == TASK_SUCCESS)
+			tact_fsm_case = 615;
+		break;
+	case 615:
+		cur_task = move_on_dir(250, BACKWARD, 0.5, NO_SENS); //280
+		if (cur_task == TASK_SUCCESS)
+			tact_fsm_case = 620;
+		break;
+	case 620:
+			if (get_tact_num_ptr()->side)	// plava
+				cur_task = task_sprat_3_1_full(BACKWARD);
+			else
+				cur_task = task_sprat_3_1_full(FORWARD);
+			//cur_task = task_sprat_3_full (BACKWARD);
+			if (cur_task == TASK_SUCCESS)
+				tact_fsm_case = 630;
+			break;
+
+
+
 
 
 	}
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -86,7 +86,8 @@ int main(void)
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 
-HAL_Init();
+
++	HAL_Init();
 
   /* USER CODE BEGIN Init */
 
